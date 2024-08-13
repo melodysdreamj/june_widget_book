f77d1bf83f8c5f76dbb98489842771ef268f5fea
june lee <melodysdreamj@gmail.com>
2024-08-13 16:06:32 +0900

    ₩

--- lib/app/widgets.dart
+++ lib/app/widgets.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:tuple/tuple.dart';
 
+import '../../widget_book/request_ad_permission_page_lego/_/_.dart' as request_ad_permission_page_lego;
+import '../../widget_book/profile_view_2_lego/_/_.dart' as profile_view_2_lego;
+import '../../widget_book/google_drive_main_view_lego/_/_.dart' as google_drive_main_view_lego;
+import '../../widget_book/music_player_page_lego/_/_.dart' as music_player_page_lego;
+import '../../widget_book/ios_notes_view_lego/_/_.dart' as ios_notes_view_lego;
+import '../../widget_book/youtube_main_view_lego/_/_.dart' as youtube_main_view_lego;
+import '../../widget_book/whatsapp_main_view_lego/_/_.dart' as whatsapp_main_view_lego;
+import '../../widget_book/insta_style_feed_lego/_/_.dart' as insta_style_feed_lego;
+import '../../widget_book/netflix_intro_page_lego/_/_.dart' as netflix_intro_page_lego;
+import '../../widget_book/spotify_main_view_lego/_/_.dart' as spotify_main_view_lego;
+import '../../widget_book/money_transfer_view_lego/_/_.dart' as money_transfer_view_lego;
+import '../../widget_book/gmail_main_view_lego/_/_.dart' as gmail_main_view_lego;
+import '../../widget_book/telegram_main_lego/_/_.dart' as telegram_main_lego;
+import '../../widget_book/facebook_messager_main_view_lego/_/_.dart' as facebook_messager_main_view_lego;
+import '../../widget_book/chat_app_view_detail_lego/_/_.dart' as chat_app_view_detail_lego;
+import '../../widget_book/request_notification_permission_page_lego/_/_.dart' as request_notification_permission_page_lego;
+import '../../widget_book/kakao_chatroom_list_lego/_/_.dart' as kakao_chatroom_list_lego;
+import '../../widget_book/login_and_register_page_lego/_/_.dart' as login_and_register_page_lego;
+import '../../widget_book/ios_reminder_view_lego/_/_.dart' as ios_reminder_view_lego;
+import '../../widget_book/music_list_page_lego/_/_.dart' as music_list_page_lego;
+import '../../widget_book/chat_app_view_lego/_/_.dart' as chat_app_view_lego;
 import '../../widget_book/listview_delete_animation_demo_lego/_/_.dart' as listview_delete_animation_demo_lego;
 import '../../widget_book/world_time_lego/_/_.dart' as world_time_lego;
 import '../../widget_book/reorderable_listview_demo_lego/_/_.dart' as reorderable_listview_demo_lego;
@@ -22,6 +43,27 @@ import '../../widget_book/toss_intro_page_lego/_/_.dart' as toss_intro_page_lego
 import '../../widget_book/hello_world_page_lego/_/_.dart' as hello_world_page_lego;
 
 final List<Tuple2<String, Widget>> widgets = [
+  Tuple2('request_ad_permission_page_lego', request_ad_permission_page_lego.NewView()),
+  Tuple2('profile_view_2_lego', profile_view_2_lego.NewView()),
+  Tuple2('google_drive_main_view_lego', google_drive_main_view_lego.NewView()),
+  Tuple2('music_player_page_lego', music_player_page_lego.NewView()),
+  Tuple2('ios_notes_view_lego', ios_notes_view_lego.NewView()),
+  Tuple2('youtube_main_view_lego', youtube_main_view_lego.NewView()),
+  Tuple2('whatsapp_main_view_lego', whatsapp_main_view_lego.NewView()),
+  Tuple2('insta_style_feed_lego', insta_style_feed_lego.NewView()),
+  Tuple2('netflix_intro_page_lego', netflix_intro_page_lego.NewView()),
+  Tuple2('spotify_main_view_lego', spotify_main_view_lego.NewView()),
+  Tuple2('money_transfer_view_lego', money_transfer_view_lego.NewView()),
+  Tuple2('gmail_main_view_lego', gmail_main_view_lego.NewView()),
+  Tuple2('telegram_main_lego', telegram_main_lego.NewView()),
+  Tuple2('facebook_messager_main_view_lego', facebook_messager_main_view_lego.NewView()),
+  Tuple2('chat_app_view_detail_lego', chat_app_view_detail_lego.NewView()),
+  Tuple2('request_notification_permission_page_lego', request_notification_permission_page_lego.NewView()),
+  Tuple2('kakao_chatroom_list_lego', kakao_chatroom_list_lego.NewView()),
+  Tuple2('login_and_register_page_lego', login_and_register_page_lego.NewView()),
+  Tuple2('ios_reminder_view_lego', ios_reminder_view_lego.NewView()),
+  Tuple2('music_list_page_lego', music_list_page_lego.NewView()),
+  Tuple2('chat_app_view_lego', chat_app_view_lego.NewView()),
   Tuple2('listview_delete_animation_demo_lego', listview_delete_animation_demo_lego.NewView()),
   Tuple2('world_time_lego', world_time_lego.NewView()),
   Tuple2('calculator_ui_lego', calculator_ui_lego.NewView()),
